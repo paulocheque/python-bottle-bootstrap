3a56c61b6a2c7d40f9a98bfa13d9df92060fcd08
paulocheque <paulocheque@gmail.com>
2015-06-14 04:35:52 -0300

    Better layout. header and footer. columns, tooltips.. angular bug fixes..

--- static/app.js
+++ static/app.js
@@ -19,27 +19,57 @@ angular.module('project', ['ui'])
     $httpProvider.defaults.headers.common['X-CSRFToken'] = $('input[name=csrfmiddlewaretoken]').val();
 }])
 
-.controller('ProjectCtrl', function($scope, $http, $window) {
-    var today = new Date();
-    var day = today.getDate();
-    var month = today.getMonth()+1; //January is 0!
-    var year = today.getFullYear();
+.config(['$sceDelegateProvider', function ($sceDelegateProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist(['self', new RegExp('^(http[s]?):\/\/.+$')]);
+}])
 
-    $scope.date = year + "/" + month + "/" + day;
-    $scope.nextUpdate = "06:00"
+.controller('ProjectCtrl', function($scope, $http, $window) {
+    $scope.lastDay = null;
+    $scope.currentDay = null;
+    $scope.nextDay = null;
+    $scope.lastUpdate = null;
+    $scope.nextUpdate = null;
+
     $scope.links = []
     $scope.messages = []
 
-    $http.get("/api/v1/links/" + year + "/" + month + "/" + day).success(function (data) {
-        console.log(data)
-        $scope.links = data
-        // data.forEach(function(d) {
-        //     $scope.data.push(d)
-        // })
-    });
-
-    $scope.getViews = function(views) {
-        return views + Math.floor((Math.random() * 5000) + 1000);
+    $scope.getLinks = function(date) {
+        var day = date.getDate();
+        var month = date.getMonth()+1; //January is 0!
+        var year = date.getFullYear();
+        $http.get("/api/v1/links/" + year + "/" + month + "/" + day).success(function (data) {
+            console.log(data)
+            $scope.links = JSON.parse(data['links'])
+            $scope.currentDay = new Date(data['currentDay'])
+            $scope.lastDay = new Date(data['lastDay'])
+            $scope.nextDay = new Date(data['nextDay'])
+            $scope.lastUpdate = new Date(data['lastUpdate'])
+            $scope.nextUpdate = new Date(data['nextUpdate'])
+        });
     }
 
+    var today = new Date()
+    $scope.getLinks(today)
+
+    // Keyboard shortcuts
+
+    document.onkeydown = function(evt) {
+        evt = evt || window.event;
+        switch (evt.keyCode) {
+            case 37:
+                leftArrowPressed();
+                break;
+            case 39:
+                rightArrowPressed();
+                break;
+        }
+    };
+
+    function leftArrowPressed() {
+        console.log('left')
+    }
+
+    function rightArrowPressed() {
+        console.log('right')
+    }
 })
